e8c64e30439b4868e81d971ae70b897b5440acc2
GodWindows <kanlinsougodwin@gmail.com>
2023-11-20 23:53:11 +0100

    We now have a GUI which opens unless you pass a valid folder as argument to the program.

--- install_fonts.py
+++ install_fonts.py
@@ -1,9 +1,11 @@
 import os
 import ctypes
 import ctypes.wintypes
-import argparse
+import sys
+import tkinter as tk
 import zipfile
 from py7zr import SevenZipFile
+from tkinter import filedialog
 
 def unzip_files_in_directory(directory):
     zip_extensions = (".zip", ".7z") 
@@ -55,21 +57,66 @@ def install_fonts_in_directory(directory):
     wm_fontchange = 0x001D
     ctypes.windll.user32.SendMessageW(hwnd_broadcast, wm_fontchange, 0, 0)
 
+def choose_folder(label):
+    # Open a dialog for selecting a folder
+    folder_path = filedialog.askdirectory()
+    
+    # Check if a folder was selected
+    if folder_path:
+        print(f"Selected Folder: {folder_path}")
+        label.config(text=f"Selected Folder: {folder_path}")
+    else:
+        print("No folder selected.")
+
+def gui():
+    root = tk.Tk()
+    root.title("Font Fiesta Unzipper And Installer Extravaganza 🎉🔤")
+    # Set the initial size of the window
+    root.geometry("800x400")
+
+    # Create a label widget
+    label = tk.Label(root, text="Click on the button below and choose the folder where your fonts files are (even if they are compressed)")
+    label.pack(pady=10)
+
+    # Create a label to show the selected directory's path
+    dir = tk.Label(root, text=" ")
+    # Create a button widget
+    button = tk.Button(root, text="Choose Folder", command=lambda: choose_folder(dir))
+    button.pack()
+
+    dir.pack(pady=10)
+
+    # Start the Tkinter event loop
+    root.mainloop()
+    
+
 def main():
-    parser = argparse.ArgumentParser(description="Install fonts from a specified folder.")
-    parser.add_argument("folder_path", help="Path to the folder containing font files (.ttf, etc.)")
-
-    args = parser.parse_args()
-
-    folder_path = args.folder_path
-
-    if not os.path.exists(folder_path):
-        print(f"Error: The specified folder path '{folder_path}' does not exist.")
+    # Check the number of command-line arguments
+    if len(sys.argv) == 1:
+        # Launch the GUI if no arguments were passed
+        gui()
         return
+    
+    if len(sys.argv) != 2:
+        # Launch the GUI if arguments are invalid
+        print("Invalid arguments. Launching GUI.")
+        gui()
+        return
+    
+    folder_path = sys.argv[1]
 
-    unzip_files_in_directory(folder_path)
+    # Check if the specified folder exists
+    if not os.path.exists(folder_path):
+        # Launch the GUI if the folder doesn't exist
+        print(f"Error: The specified folder path '{folder_path}' does not exist. Launching GUI.")
+        gui()
+    else:
+        # Valid arguments. Proceeding to unzipping and font installng without launching GUI.
+        unzip_files_in_directory(folder_path)
+        install_fonts_in_directory(folder_path)
 
-    install_fonts_in_directory(folder_path)
+
+
 
 if __name__ == "__main__":
     main()
